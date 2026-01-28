760ddecfbbf518802dfd3e98845ccfd02024ab6b
Denis <deniskamaldinov85@gmail.com>
2026-01-28 11:55:38 +0500

    Added module WebSoocketChat, interface IWebSocket

--- client/src/scripts/WebSokets.ts
+++ client/src/scripts/WebSokets.ts
@@ -1,11 +1,11 @@
-import {useRef} from "react";
-
+import SERVER_URL from "../config";
 
 const createConnection = () => {
-  const socket = useRef<WebSocket| null>(null);
+  const socket = new WebSocket(SERVER_URL);
 
-
-
-  
+  return () => {
+    socket?.close();
+  };
 }
 
+export default createConnection;
