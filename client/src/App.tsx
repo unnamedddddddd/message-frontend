c8322e86b42e60e5d21657c50bd28a65a1204a80
denis <deniskamaldinov85@gmail.com>
2026-01-30 11:01:54 +0500

    Добавил форму Login, изменил палитру цветов.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './components/HomeForm/Home'
 import './App.css'
+import Login from './components/LoginForm/Login'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
---
+++ client/src/components/LoginForm/Login.tsx
@@ -0,0 +1,42 @@
+import './Login.css'
+
+const Login = () => {
+
+
+  return(
+    <>
+      <div className="login-main">
+        <div className="login-form-container">
+          <form action="" className="login-form">
+            <div className="main-form">
+              <div className="input-user-data">
+                <div className="input-login">
+                  <label htmlFor="" className="login-label">
+                    Login
+                  </label>
+                  <input type="text" className="login" placeholder="Enter Login"/>
+                </div>
+                <div className="input-password">
+                   <label htmlFor="" className="login-label">
+                    Password
+                  </label>
+                  <input type="password" className="password" placeholder="Enter Password"/>
+                </div>
+                <div className="remember-me">
+                </div>
+                <div className="button-container">
+                  <button className='login-button'>
+                    Sign in
+                  </button> 
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+} 
+
+
+export default Login;
