fe08a63b28e515045117a03c074850dbcf3bfb63
denis <deniskamaldinov85@gmail.com>
2026-04-27 17:54:34 +0500

    feat(ui): invite server widget

--- client/src/components/chat/FindedServer.tsx
+++ client/src/components/chat/FindedServer.tsx
@@ -16,29 +16,32 @@ const FindedServer = ({ serverId, avatar, name }: ServerProps) => {
       return;
     }
     setStatus(true);
-     addNotification('success', 'Приглашение отправлено, ожидайте ответа админа');
+    addNotification('success', 'Приглашение отправлено, ожидайте ответа админа');
   }
 
   return (
-    <button 
-      className="group mb-5 w-fit p-2 bg-[#353536]/70 border border-[#6d7275]/40 rounded-xl transition-all duration-200 
-              hover:bg-[#414243]/90 active:bg-[#4e4f51]/90 
-                disabled:opacity-60 disabled:cursor-not-allowed disabled:grayscale-[0.5]"
-      title={name}
-      onClick={handleInviteServer}
-      disabled={status}
-    >
-      <div className="flex items-center gap-2"> 
-        <img 
-          src={`${SERVER_URL}${avatar}`} 
-          alt={name} 
-          className="w-12 h-12 rounded-[15px] border border-[#6d7275]/30 object-cover" 
-        />
-        <div className="">
-          <span>{name}</span>
+    <div className="flex items-center p-2 border rounded-2xl border-[#6d7275]/40 justify-between">
+        <div className="flex items-center gap-2"> 
+          <img 
+            src={`${SERVER_URL}${avatar}`} 
+            alt={name} 
+            className="w-12 h-12 rounded-[15px] border border-[#6d7275]/30 object-cover" 
+          />
+          <div className="">
+            <span>{name}</span>
+          </div>
         </div>
-      </div>
-    </button>
+        <button 
+        className="w-fit p-2 bg-[#353536]/70 border border-[#6d7275]/40 rounded-xl transition-all duration-200 
+                hover:bg-[#414243]/90 active:bg-[#4e4f51]/90 
+                  disabled:opacity-60 disabled:cursor-not-allowed disabled:grayscale-[0.5]"
+        title={name}
+        onClick={handleInviteServer}
+        disabled={status}
+      >
+        Присоединится к серверу?
+      </button>
+    </div>
   )
 };
 
