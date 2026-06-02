80e5d66cc6ad26ead879113cfbafe61e78950ea6
Denis <deniskamaldinov85@gmail.com>
2026-06-02 21:47:19 +0500

    fix: websocket url config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-export const WEBSOCKET_URL = 'https://message-backend-production-989b.up.railway.app';
+export const WEBSOCKET_URL = 'ws://message-backend-production-989b.up.railway.app';
 export const SERVER_URL = 'https://message-backend-production-989b.up.railway.app';
